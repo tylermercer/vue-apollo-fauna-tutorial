8cc122dd77d7a77c62070c9f4b510f87b998377b
Tyler Mercer <tyleralanmercer@gmail.com>
2020-02-20 19:57:53 -0700

    Remove comments

--- src/components/NotesList.vue
+++ src/components/NotesList.vue
@@ -1,19 +1,12 @@
 <template>
   <ApolloQuery :query="gql => gql(query)" :variables="{ pageSize }">
     <template v-slot="{ result: { error, data }, isLoading }">
-      <!-- Loading -->
       <div v-if="isLoading">Loading...</div>
-
-      <!-- Error -->
       <div v-else-if="error">An error occurred</div>
-
-      <!-- Result -->
       <div v-else-if="data">
         <Note v-for="post in data.allNotes.data" :key="post._id" :body="post.body" :author="post.author"/>
         <p>{{data.allNotes.data.length}} notes</p>
       </div>
-
-      <!-- No result -->
       <div v-else class="no-result">No result :(</div>
     </template>
   </ApolloQuery>
